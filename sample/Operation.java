e73e921ead10eadd79008d503079f1ee8ed75fe7
mskarthi95 <Trainee@4FTECHTEAM8138.kgfsl.com>
2017-06-01 18:04:14 +0530

    day 1

---
+++ sample/Operation.java
@@ -0,0 +1,18 @@
+public class Operation{
+
+public static void main(String[] args){
+
+int a = 5 ;
+int b = 10;
+double c = 10.23;
+
+
+//Start to print
+System.out.println("\nOperation values");
+/* new /n line */
+
+System.out.println("\nThe value of Integer in int :\n" + a);
+
+}
+
+}

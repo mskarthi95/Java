7489d16d6b849a11fdf2533850aeb89e10601158
mskarthi95 <Trainee@4FTECHTEAM8138.kgfsl.com>
2017-06-02 17:25:23 +0530

    day 2

---
+++ Package/src/Pack/Mypack.java
@@ -0,0 +1,10 @@
+package Pack;
+import Pack.*;
+
+public class Mypack {
+
+	public static void main(String args[]){
+		Pack Obj = new Pack();
+		Obj.msg();
+	}
+}
---
+++ Package/src/Pack/Pack.java
@@ -0,0 +1,11 @@
+package Pack;
+
+public class Pack {
+
+	public void msg()
+	{
+		System.out.println("");
+		System.out.println("package is working successfully");
+
+	}
+}

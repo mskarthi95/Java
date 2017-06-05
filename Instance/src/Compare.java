7575356587a67199b7da2740e9f6cdcda3b6549b
mskarthi95 <Trainee@4FTECHTEAM8138.kgfsl.com>
2017-06-05 15:29:51 +0530

    Day 3

---
+++ Instance/src/Compare.java
@@ -0,0 +1,9 @@
+
+public class Compare {
+public static void main (String args[]){
+	Compare c= new Compare();
+	System.out.println(c instanceof Compare);
+	Compare e= c;
+	System.out.println(e instanceof Compare);
+	}
+}

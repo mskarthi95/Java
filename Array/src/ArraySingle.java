a7248561becbae97ec079b3bcbe2788c2ef3c157
mskarthi95 <Trainee@4FTECHTEAM8138.kgfsl.com>
2017-06-07 14:07:07 +0530

    Day 5

--- Array/src/ArraySingle.java
+++ Array/src/ArraySingle.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 
 public class ArraySingle {
 public static void main (String args[]){
@@ -10,6 +12,12 @@ public static void main (String args[]){
 	a[4]=50;
 	
 	for(int i=0;i<a.length;i++)
-		System.out.println(a[i]);		
+		System.out.println(a[i]);	
+	
+	String[] array={"10","20","30"};
+	
+	System.out.println(Arrays.toString(array));
+	
+	System.out.println(Arrays.asList(array));
 }
 }
---
+++ Methods/src/TestVoid.java
@@ -0,0 +1,18 @@
+
+public class TestVoid {
+public static void main(String args[]){
+	print(45);
+}
+
+public static void print(int score)
+{
+	if(score < 20){
+System.out.println("less than 20");	
+	}
+	else
+	{
+		System.out.println("Greater then 20");
+	}
+
+}
+}
